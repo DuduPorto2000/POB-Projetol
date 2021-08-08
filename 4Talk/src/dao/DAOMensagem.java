a8b1d5fb96b956597b43f0a64ade033256245846
Eduardo Porto <60082543+DuduPorto2000@users.noreply.github.com>
2021-08-08 20:31:10 -0300

    Add files via upload

---
+++ 4Talk/src/dao/DAOMensagem.java
@@ -0,0 +1,22 @@
+package dao;
+import java.util.List;
+
+import com.db4o.query.Query;
+
+import modelo.Mensagem;
+
+public class DAOMensagem  extends DAO<Mensagem> {
+	
+	public Mensagem read(Object chave) {
+		int id = (Integer) chave;
+		Query q = manager.query();
+		q.constrain(Mensagem.class);
+		q.descend("id").constrain(id);
+		List<Mensagem> result = q.execute();
+		if (result.size() > 0)
+			return result.get(0);
+		else
+			return null;
+	}
+	
+}
---
+++ 4Talk/src/dao/DAOUsuario.java
@@ -0,0 +1,25 @@
+package dao;
+
+
+
+import java.util.List;
+
+import com.db4o.query.Query;
+
+import modelo.Usuario;
+
+public class DAOUsuario extends DAO<Usuario> {
+
+	public Usuario read(Object chave) {
+	String nome = (String) chave;
+	Query q = manager.query();
+	q.constrain(Usuario.class);
+	q.descend("nome").constrain(nome);
+	List<Usuario> result = q.execute();
+	if (result.size() > 0)
+		return (Usuario) result.get(0);
+	else
+		return null;
+	}
+
+}
